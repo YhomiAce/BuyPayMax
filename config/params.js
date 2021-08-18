b2dcc15d4adf8e3ef0517b629b51161ff8cfecac
Kareem Yomi <kareemyomi91@gmail.com>
2021-08-18 10:23:54 +0100

    change

--- config/params.js
+++ config/params.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 module.exports = {
     // LOCAL_PORT: 3000,
     NODE_ENV: "development",
@@ -8,14 +9,13 @@ module.exports = {
     //PAYSTACK_SECRET: "sk_live_cd2de91a6b91bb0d0735034e35cae3db42f0ee8f",
     CLIENT_ID_FB: "803481943564861",
     CLIENT_SECRET_FB: "1021bfb85276588bf79271cb2712f9cc",
-    APP_NAME: "Tobby EpayCoin",
-    // SITE_URL: "https://tobby-epay.herokuapp.com",
-    SITE_URL: "http:localhost:5000",
+    APP_NAME: process.env.APP_NAME,
+    SITE_URL: process.env.SITE_URL,
     CLIENT_ID_GOOGLE: "666290590783-djcj0jd6q5g7e00ovciko1elct84qrij.apps.googleusercontent.com",
     CLIENT_SECRET_GOOGLE: "X-aoQVXrMZ-tA3EdRPsCwpBh",
-    EMAIL_PASSWORD: "e841d92282037e",
-    EMAIL_USERNAME: "1e544c5e5f7d79",
-    EMAIL_HOST: 'smtp.mailtrap.io',
-    EMAIL_PORT: "587",
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
+    EMAIL_USERNAME: process.env.EMAIL_USERNAME,
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_PORT: process.env.EMAIL_PORT,
     
 };
