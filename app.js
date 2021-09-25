4c100dbc12e0a32c77747d8de0bf72803a631ad0
Kareem Yomi <kareemyomi91@gmail.com>
2021-09-25 09:01:32 +0100

    changes to cron job

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ io.on("connection", socket => {
 });
 
 // scheduler task and all
-cron.schedule("00 6 * * *", () => {
+cron.schedule("30 9 * * *", () => {
    
     // if(shell.exec("node cronjob.js").code !== 0) {
     //     console.log("something went wrong");
