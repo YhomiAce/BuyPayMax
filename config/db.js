c3ca1cf9dea10001c642a8202e5437fdb3ed682c
Kareem Yomi <kareemyomi91@gmail.com>
2021-10-19 16:47:56 +0100

    api for paybuymax

--- config/db.js
+++ config/db.js
@@ -6,8 +6,13 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
 
 });
 
-db.authenticate().then(()=>{
-  console.log('database connected')
-}).catch((err)=>console.log(err))
+try {
+  db.authenticate().then(()=>{
+    console.log('database connected')
+  }).catch((err)=>console.log(err))
+} catch (error) {
+  console.log(error);
+  process.exit(1)
+}
 
 module.exports = db;
