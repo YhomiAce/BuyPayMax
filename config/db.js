0356b5da2550f47f0147f5ef81442df123841788
Kareem Yomi <kareemyomi91@gmail.com>
2021-10-19 17:10:16 +0100

    api for paybuymax

--- config/db.js
+++ config/db.js
@@ -1,9 +1,34 @@
+require("dotenv").config();
 const Sequelize = require('sequelize');
+
+
+class CustomDecimal extends Sequelize.DataTypes.DECIMAL {
+  static parse(value) {
+    return parseFloat(value);
+  }
+}
+
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,{
   host:'localhost',
   dialect:'mysql',
-
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 60000,
+    idle: 10000
+  },
+  dialectOptions: {
+    connectTimeout: 80000
+  },
+  hooks: {
+    afterConnect() {
+      const dTypes = {
+        DECIMAL: CustomDecimal
+      };
+      this.connectionManager.refreshTypeParser(dTypes);
+    }
+  }
 });
 
 db.authenticate().then(()=>{
